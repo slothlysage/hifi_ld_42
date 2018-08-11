b90041b71ada308ba0310fba4450f1e25c4c3453
Robin Wilson <robin.kath.wilson@gmail.com>
2018-08-11 12:47:30 -0700

    added motorVelocity function

---
+++ locomotion/Add-Avatar-MotorVelocity.js
@@ -0,0 +1,21 @@
+// Adds Avatar movement velocity for Cube-Pooper Motion!
+// Uses head movements to move forward. Should be used in the air.
+
+(function () {
+
+    var MOTOR_VELOCITY = {x: 0, y: 0, z: 3};
+    var MOTOR_TIMESCALE = 0.5;
+
+    MyAvatar.motorVelocity = MOTOR_VELOCITY;
+    MyAvatar.motorTimescale = MOTOR_TIMESCALE;
+
+
+    function scriptEnding() {
+        console.log("### in script ending");
+        MyAvatar.motorVelocity = {x: 0, y: 0, z: 3};
+        MyAvatar.motorTimescale = 0;
+    }    
+
+    Script.scriptEnding.connect(scriptEnding);
+
+})();
